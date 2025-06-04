5cb799b7d1a5289c342d53eb4e0652bac5b44052
blainecurren <blaine.curren@gmail.com>
2025-06-04 10:04:52 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to PDFTitan</h1>
+        <p>Your PDF management solution</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
